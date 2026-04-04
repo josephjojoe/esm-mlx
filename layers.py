6a7a65c13096877ea61a151ce0b193d9f306dca4
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2026-04-04 15:54:33 -0400

    add layers.py stub for multi-head attention and the transformer layer

---
+++ layers.py
@@ -0,0 +1,16 @@
+import mlx.core as mx
+import mlx.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, x):
+        pass
+
+class TransformerLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, x):
+        pass
