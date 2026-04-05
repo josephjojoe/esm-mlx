a76486cc33b4bc50e91cd92261f468d6f45b5119
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2026-04-04 23:00:16 -0400

    init for mha

--- layers.py
+++ layers.py
@@ -2,8 +2,19 @@ import mlx.core as mx
 import mlx.nn as nn
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, hidden_dim, num_heads):
         super().__init__()
+        self.hidden_dim = hidden_dim
+        self.num_heads = num_heads
+
+        self.head_dim = hidden_dim // num_heads
+        self.scale = self.head_dim ** -0.5
+
+        self.q_proj = nn.Linear(hidden_dim, hidden_dim)
+        self.k_proj = nn.Linear(hidden_dim, hidden_dim)
+        self.v_proj = nn.Linear(hidden_dim, hidden_dim)
+        self.out_proj = nn.Linear(hidden_dim, hidden_dim)
+        self.rope = nn.RoPE(self.head_dim, traditional=False, base=10000)
 
     def __call__(self, x):
         pass
